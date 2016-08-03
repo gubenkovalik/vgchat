cce98ed623b88f1bb5443ace80a4d6cd147fb0e6
Джен Кот <gubenkovalik@gmail.com>
2016-08-03 22:05:03 +0300

    Update

--- app/Http/SocketIO.php
+++ app/Http/SocketIO.php
@@ -1,23 +1,7 @@
 <?php
 
 namespace App\Http;
-require_once("../vendor/wisembly/elephant.io/src/Client.php");
 
-require_once("../vendor/wisembly/elephant.io/src/Client.php");
-require_once("../vendor/wisembly/elephant.io/src/EngineInterface.php");
-require_once("../vendor/wisembly/elephant.io/src/AbstractPayload.php");
-require_once("../vendor/wisembly/elephant.io/src/Exception/SocketException.php");
-require_once("../vendor/wisembly/elephant.io/src/Exception/MalformedUrlException.php");
-require_once("../vendor/wisembly/elephant.io/src/Exception/ServerConnectionFailureException.php");
-require_once("../vendor/wisembly/elephant.io/src/Exception/UnsupportedActionException.php");
-require_once("../vendor/wisembly/elephant.io/src/Exception/UnsupportedTransportException.php");
-
-require_once("../vendor/wisembly/elephant.io/src/Engine/AbstractSocketIO.php");
-require_once("../vendor/wisembly/elephant.io/src/Engine/SocketIO/Session.php");
-require_once("../vendor/wisembly/elephant.io/src/Engine/SocketIO/Version0X.php");
-require_once("../vendor/wisembly/elephant.io/src/Engine/SocketIO/Version1X.php");
-require_once("../vendor/wisembly/elephant.io/src/Payload/Decoder.php");
-require_once("../vendor/wisembly/elephant.io/src/Payload/Encoder.php");
 
 use ElephantIO\Client as Elephant;
 use ElephantIO\Engine\SocketIO\Version1X;
